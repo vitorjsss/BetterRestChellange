8e9f75b03373ad05b1770227e145ad529f7e1915
Vitor <vitorjsssmamede.1@gmail.com>
2024-04-09 13:53:11 -0300

    Initial Commit

---
+++ BetterRestChellange/BetterRestChellangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestChellangeApp.swift
+//  BetterRestChellange
+//
+//  Created by Vitor on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestChellangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
